d4d96d39cc5dab25a21fa2ff2b7750956c2f8d54
CHIMPMAN-CHIMP <tomess123@outlook.com>
2026-08-15 01:06:33 +0930

    i am trying to manually figure ts out - attempts to add next stop visibility

--- public/getNextStop.js
+++ public/getNextStop.js
@@ -19,6 +19,10 @@ function getNextStop() {
             closest_diff = diff
         }
     }
-    console.log(closest_index) // <-- returns NaN, logic error most likely
-    return cache.closestVehicle.stops[closest_index].stop_name
+
+    const view = document.querySelector('#next-stop')
+    view.style.setProperty('--stop-name', `${cache.closestVehicle.stops[closest_index].stop_name}`)
+
+    // console.log(closest_index) // <-- returns NaN, logic error most likely
+    // return cache.closestVehicle.stops[closest_index].stop_name
 }
