ad94f9f22d404dfe3edfe7d579f3013cffb7fbb4
CHIMPMAN-CHIMP <tomess123@outlook.com>
2026-08-15 01:32:12 +0930

    no effin way

--- public/getNextStop.js
+++ public/getNextStop.js
@@ -21,7 +21,7 @@ function getNextStop() {
     }
 
     const view = document.querySelector('#next-stop')
-    view.style.setProperty('--stop-name', `"${cache.closestVehicle.stops[closest_index].stop_name}"`)
+    view.style.setProperty('--next-stop', `"${cache.closestVehicle.stops[closest_index].stop_name}"`)
 
     // console.log(closest_index) // <-- returns NaN, logic error most likely
     // return cache.closestVehicle.stops[closest_index].stop_name lol
